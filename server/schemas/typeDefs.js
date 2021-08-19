684e2dab1463e017d06cf46b10e172ce664c250c
dhuggins221 <dhuggins221@gmail.com>
2021-08-18 19:55:53 -0500

    first commit

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,5 +1,21 @@
 const { gql } = require("apollo-server-express");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+    type Mutation {
+        login(email: String!, password: String!): User
+        addUser(username: String!, email: String!, password: String!): User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        me: User
+        users: [User]
+        user(username: String!): User
+      }
+`;
 
 module.exports = typeDefs;
