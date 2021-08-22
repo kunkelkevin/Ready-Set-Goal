9faaa77cfe9c0a6d4cc5be66b51ab8ff5f50c0a7
Rarmstrong45 <Ryan_armstrong07@yahoo.com>
2021-08-22 15:18:04 -0500

    updated sign up and sign in pages - added link to sign up page from login page

--- client/src/components/Signin/index.js
+++ client/src/components/Signin/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FormButton, FormH1, FormInput, FormLabel, Container, Form, FormContent, Text, Icon, FormWrap, FormGrid } from './SigninElements'
+import { FormButton, FormH1, FormInput, FormLabel, Container, Form, FormContent, SignUp, Icon, FormWrap, FormGrid } from './SigninElements'
 
 const SignIn = () => {
     return (
@@ -7,7 +7,6 @@ const SignIn = () => {
         <Container>
             <FormWrap>
                 <Icon to="/">Ready! Set! Goal!</Icon>
-                <FormGrid>
                 <FormContent>
                     <Form action="#">
                         <FormH1>Sign in to your account</FormH1>
@@ -16,21 +15,9 @@ const SignIn = () => {
                         <FormLabel htmlFor='password'>Password</FormLabel>
                         <FormInput type='password' required />
                         <FormButton typle='submit'>Sign In</FormButton>
-                        <Text>Forgot Password</Text>
+                        <SignUp to="/signup">Need to Sign Up? Click Here</SignUp>
                     </Form>
                 </FormContent>
-                <FormContent>
-                    <Form action="#">
-                        <FormH1>Sign Up to Play</FormH1>
-                        <FormLabel htmlFor='for'>Email</FormLabel>
-                        <FormInput type='email' required />
-                        <FormLabel htmlFor='password'>Password</FormLabel>
-                        <FormInput type='password' required />
-                        <FormButton typle='submit'>Sign In</FormButton>
-                        <Text>Verify Account</Text>
-                    </Form>
-                </FormContent>
-                </FormGrid>
             </FormWrap>
         </Container>    
         </>
