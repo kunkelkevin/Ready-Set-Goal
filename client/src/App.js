3bd1591fed19723ffd7c42ea0e320115c86cb5ff
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-08-23 14:34:32 -0500

    reset database connection to work on front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,41 @@
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { ApolloProvider } from '@apollo/react-hooks';
+import ApolloClient from 'apollo-boost';
+
 import Home from "./pages";
-import SigninPage from "./pages/signin";
-import SignupPage from "./pages/signup";
-import { ApolloProvider } from "@apollo/react-hooks";
-import ApolloClient from "apollo-boost";
+import SigninPage from "./pages/Signin";
+import SignupPage from "./pages/Signup";
+import { StoreProvider } from "./utils/GlobalState";
 
 const client = new ApolloClient({
   request: (operation) => {
-    const token = localStorage.getItem("id_token");
+    const token = localStorage.getItem('id_token')
     operation.setContext({
       headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    });
+        authorization: token ? `Bearer ${token}` : ''
+      }
+    })
   },
-  uri: "/graphql",
-});
+  uri: '/graphql',
+})
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <Switch>
-          <Route path="/" component={Home} exact />
-          <Route path="/signin" component={SigninPage} exact />
-          <Route path="/signup" component={SignupPage} exact />
-        </Switch>
+        <div>
+          <StoreProvider>
+            <Switch>
+              <Route path="/" component={Home} exact />
+              <Route path="/signin" component={SigninPage} exact />
+              <Route path="/signup" component={SignupPage} exact />
+            </Switch>
+          </StoreProvider>
+        </div>
       </Router>
     </ApolloProvider>
+
   );
 }
 
