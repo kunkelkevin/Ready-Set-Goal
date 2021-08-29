c5b2b906f5731cc07a139340581252801913a162
Kevin Kunkel <kunkelkevin@yahoo.com>
2021-08-29 17:10:39 -0500

    took out unneccessary code

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,15 +1,5 @@
 import { useReducer } from "react";
-import {
-  UPDATE_FIELDS,
-  UPDATE_GAMES,
-  UPDATE_CART_QUANTITY,
-  REMOVE_FROM_CART,
-  ADD_MULTIPLE_TO_CART,
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
-  CLEAR_CART,
-  TOGGLE_CART,
-} from "./actions";
+import { UPDATE_FIELDS, UPDATE_GAMES } from "./actions";
 
 export const reducer = (state, action) => {
   switch (action.type) {
@@ -23,60 +13,6 @@ export const reducer = (state, action) => {
       return {
         ...state,
         games: [...action.games],
-      };
-
-    case ADD_MULTIPLE_TO_CART:
-      return {
-        ...state,
-        cart: [...state.cart, ...action.products],
-      };
-
-    case UPDATE_CART_QUANTITY:
-      return {
-        ...state,
-        cartOpen: true,
-        cart: state.cart.map((product) => {
-          if (action._id === product._id) {
-            product.purchaseQuantity = action.purchaseQuantity;
-          }
-          return product;
-        }),
-      };
-
-    case REMOVE_FROM_CART:
-      let newState = state.cart.filter((product) => {
-        return product._id !== action._id;
-      });
-
-      return {
-        ...state,
-        cartOpen: newState.length > 0,
-        cart: newState,
-      };
-
-    case CLEAR_CART:
-      return {
-        ...state,
-        cartOpen: false,
-        cart: [],
-      };
-
-    case TOGGLE_CART:
-      return {
-        ...state,
-        cartOpen: !state.cartOpen,
-      };
-
-    case UPDATE_CATEGORIES:
-      return {
-        ...state,
-        categories: [...action.categories],
-      };
-
-    case UPDATE_CURRENT_CATEGORY:
-      return {
-        ...state,
-        currentCategory: action.currentCategory,
       };
 
     default:
